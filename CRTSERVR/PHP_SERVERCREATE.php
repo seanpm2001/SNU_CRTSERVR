a3743a3ffa020c80b4355ce22982bf228e91182a
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-01 20:47:27 -0700

    PHP_SERVERCREATE.php
    The server creation tool PHP edition. I intend to have versions for various programming languages as well, including Ruby, JavaScript, R, etc. This script currently isn't functional.

---
+++ CRTSERVR/PHP_SERVERCREATE.php
@@ -0,0 +1,21 @@
+<?php
+// server create utility
+// start of script
+/* site title */
+$serverName = ("untitled server");
+/* TOU (Terms of Use)
+You are allowed to use the SNU server creation tool to create your own server. You have to host it on your own. You can strip the site of certain priveleges to fit your region.
+You are simply providing a mirror to the site. Whatever you do is on your own, you will have to manage your own server
+You will have to modify the values in this file to continue setting up your server. Things like $serverName $YourSiteURL $Admin1 and - need to be modified
+*/
+/* variables */
+$fileCount = 200;
+$YourSiteURL = ("https://example.example.net/home");
+$Admin1 = ("your name here");
+/* first boot */
+include "";
+echo ("preparing your server");
+/* startup */
+echo ("starting up");
+// end of script
+?>
